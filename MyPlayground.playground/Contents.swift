df28c7511bf1d208713c4e0f92dd38b679427da4
stu <stu382095@gmail.com>
2023-11-21 18:37:04 +0800

    ｍｅｌｏｄｙ

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Foundation
+import UIKit
+let melodyImage = UIImage(named: "melody")
+let melodyImageView = UIImageView(image: melodyImage)
+melodyImageView.backgroundColor = UIColor(red: 0, green: 0.5, blue: 0, alpha: 1)
+
+
